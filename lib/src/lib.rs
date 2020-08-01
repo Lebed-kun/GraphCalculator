843adfcd59fe9e69229dc4228eb68c3994f27778
Lebed-kun <lebed-kun@mail.ru>
2020-08-01 20:23:24 +0300

    Change graph structure

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,16 +1,19 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 
 #[allow(non_snake_case)]
 struct Graph<'a> {
-    adjacentVerticies: &'a HashMap<
+    verticies: HashMap<
         &'a str,
-        &'a HashSet<&'a str>
-    >
+        u8
+    >,
+    adjacentMatrix: Vec<Vec<u8>>
 }
 
 impl<'a> Graph<'a> {
-    fn new() -> &Graph<'a> {
+    fn new(
+        verticies: &'a[&'a str],
+        
+    ) -> &Graph<'a> {
         
     }
 }
