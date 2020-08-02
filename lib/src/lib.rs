527ae7b7fe06bd50c297702801881cb154d49dc5
Lebed-kun <lebed-kun@mail.ru>
2020-08-02 19:55:10 +0300

    Add constructor for graph

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -9,11 +9,24 @@ struct Graph<'a> {
     adjacentMatrix: Vec<Vec<u8>>
 }
 
+#[allow(non_snake_case)]
 impl<'a> Graph<'a> {
-    fn new(
-        verticies: &'a[&'a str],
-        
-    ) -> &Graph<'a> {
-        
+    pub fn new(verticiesCount: u8) -> Box<Graph<'a>> {
+        let verticies = HashMap::new();
+
+        let mut adjacentMatrix = Vec::with_capacity(verticiesCount as usize);
+
+        for _ in 0..verticiesCount {
+            adjacentMatrix.push(
+                Vec::with_capacity(verticiesCount as usize)
+            );
+        }
+
+        let graph = Box::new(Graph {
+            verticies: verticies,
+            adjacentMatrix: adjacentMatrix
+        });
+
+        return graph;
     }
 }
