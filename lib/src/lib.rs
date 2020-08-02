44559e04147fab0fef11f6c0147e36084313bf97
Lebed-kun <lebed-kun@mail.ru>
2020-08-02 20:50:01 +0300

    Add methods for adding verticies and edges

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -29,4 +29,65 @@ impl<'a> Graph<'a> {
 
         return graph;
     }
+
+    pub fn addVertex(&mut self, vertex: &'a str) {
+        self.verticies.insert(
+            vertex,
+            self.verticies.len() as u8
+        );
+    }
+
+    fn padVertex(&mut self, vertex: &'a str) {
+        let containsVertex = self.verticies.contains_key(vertex);
+        if !containsVertex {
+            self.addVertex(vertex);
+        }
+    }
+
+    fn padAdjacents(&mut self, id: u8) {
+        let verticiesCount = self.adjacentMatrix.len();
+        if id >= verticiesCount as u8 {
+            self.adjacentMatrix.insert(
+                id as usize,
+                Vec::with_capacity(verticiesCount)
+            );
+
+            for i in 0..self.adjacentMatrix.len() {
+                self.adjacentMatrix[i].insert(
+                    verticiesCount,
+                    0
+                )
+            }
+        }
+    }
+
+    pub fn addEdge(&mut self, source: &'a str, destination: &'a str) {
+        self.padVertex(source);
+        self.padVertex(destination);
+
+        let mut sourceId: u8 = 0;
+        match self.verticies.get(&source) {
+            Some(&source) => {
+                sourceId = source
+            },
+            _ => ()
+        }
+
+        let mut destinationId: u8 = 0;
+        match self.verticies.get(&destination) {
+            Some(&destination) => {
+                destinationId = destination
+            },
+            _ => ()
+        }
+
+        self.padAdjacents(sourceId);
+        self.padAdjacents(destinationId);
+
+        self.adjacentMatrix[
+            sourceId as usize
+        ][
+            destinationId as usize
+        ] = 1;
+    }
 }
